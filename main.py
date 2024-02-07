9be86b4a384dc99cb51f306e9bf65ca331a793ff
Seek-Techs <sikiru.yusuff@yahoo.com>
2024-02-07 09:16:16 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,49 @@
+from flet import *
+
+def main(page: Page):
+    BG = '#041955'
+    FWG = '#97b4ff'
+    FG = '#3450a1'
+    PINK = '#eb06ff'
+    
+    first_page_contents = Container(
+        
+    )
+
+    
+    page_1 = Container()
+    page_2 = Row(
+        controls=[
+            Container(
+                width=400,
+                height=850,
+                bgcolor=FG,
+                border_radius=35,
+                padding=padding.only(
+                    top=50,
+                    left=20,
+                    right=20,
+                    bottom=5
+                ),
+                content=Column(
+                    controls=[
+                      first_page_contents  
+                    ]
+                )
+            ),
+        ]
+    )
+    container = Container(
+        width=500,
+        height=500, 
+        bgcolor=BG, 
+        border_radius=35,
+        content=Stack(
+            controls=[
+                page_1,
+                page_2
+            ]
+        )
+    )
+    page.add(container)
+app(target=main)
